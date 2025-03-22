0011f30eb1eebcabbc7d6626bb6c6d1e05608c1a
elmaxyto <97243490+elmaxyto@users.noreply.github.com>
2025-03-22 22:06:00 +0100

    Update unisci_epg.py

--- unisci_epg.py
+++ unisci_epg.py
@@ -7,9 +7,13 @@ root_main = tree_main.getroot()
 tree_pluto = ET.parse('pluto_tv_epg.xml')
 root_pluto = tree_pluto.getroot()
 
-# Unisci le EPG
+# Unisci i canali
 for channel in root_pluto.findall('.//channel'):
     root_main.append(channel)
+
+# Unisci i programme
+for programme in root_pluto.findall('.//programme'):
+    root_main.append(programme)
 
 # Salva l'EPG unificata
 tree_main.write('epg.xml', encoding='utf-8', xml_declaration=True)
